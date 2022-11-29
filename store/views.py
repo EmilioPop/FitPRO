a6d97fa4f4e106eb807d8856bcc13ff6aba735b3
Emilio Pop <pop_emilio@yahoo.com>
2022-11-29 18:07:08 +0200

    .

--- store/views.py
+++ store/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
+from carts.models import CartItem
+from carts.views import _cart_id
 from category.models import Category
 from store.models import Product
 
@@ -13,7 +15,8 @@ def store(request, category_slug=None):
         products = Product.objects.filter(category=categories, is_available=True)
         product_count = products.count()
     else:
-        products = Product.objects.all().filter(is_available=True)  # is_available=True --> brings only products that are in stock
+        products = Product.objects.all().filter(
+            is_available=True)  # is_available=True --> brings only products that are in stock
         product_count = products.count()
 
     context = {
@@ -22,13 +25,17 @@ def store(request, category_slug=None):
     }
     return render(request, 'store/store.html', context)
 
+
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
+
     except Exception as e:
         raise e
 
     context = {
         'single_product': single_product,
+        'in_cart': in_cart,
     }
     return render(request, 'store/product_detail.html', context)
